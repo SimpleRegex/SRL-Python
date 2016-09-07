4521deace6d978088fb0f5a1d11e8eea95d64380
soasme <soasme@gmail.com>
2016-09-07 18:02:35 +0800

    test capture for.

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -116,9 +116,6 @@ def test_rules():
     for filename in glob('tests/rules/*.rule'):
         with open(filename) as f:
             fdata = f.read()
-            rule = SRL('literally "srl:", whitespace once or more, capture (anything once or more)')
-            dsl = rule.compiled.search(fdata).groups()[0]
-            srl = SRL(dsl)
             in_capture = False
             data = {'srl': None, 'matches': [], 'no_matches': [], 'captures': {}}
             for line in fdata.splitlines():
@@ -134,11 +131,33 @@ def test_rules():
                     data['no_matches'].append(line[11:-1])
                 elif line.startswith('capture for "') and line.endswith('":'):
                     in_capture = line[13:-2]
-                    data['captures'][in_capture] = {}
                 elif in_capture and line.startswith('-'):
                     split = line[1:].split(': ')
-                    data['captures'][in_capture][split[1].strip()] = split[2][1:-1]
+                    index = int(split[0].strip())
+                    name = split[1].strip()
+                    matching = split[2][1:-1]
+                    data['captures'].setdefault(in_capture, {})
+                    if name == '0':
+                        data['captures'][in_capture].setdefault(index, [])
+                        data['captures'][in_capture][index].append(matching)
+                    else:
+                        data['captures'][in_capture].setdefault(index, {})
+                        data['captures'][in_capture][index][name] = matching
+            srl = SRL(data['srl'])
             for match in data['matches']:
                 assert srl.search(match), match
             for no_match in data['no_matches']:
                 assert not srl.search(no_match), no_match
+            for capture, matches in data['captures'].items():
+                for index, expected in matches.items():
+                    if isinstance(expected, list):
+                        assert expected[0] == srl.findall(capture)[index]
+                    else:
+                        groupindex = srl.groupindex
+                        all = srl.findall(capture)
+                        for key, val in expected.items():
+                            find_val = srl.findall(capture)[index]
+                            if isinstance(find_val, str):
+                                assert val == find_val
+                            else:
+                                assert val == find_val[groupindex[key] - 1]
